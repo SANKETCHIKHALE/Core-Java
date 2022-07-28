3bc915feb02e20140307a19bfadeff3425797daa
SANKETCHIKHALE <97874464+SANKETCHIKHALE@users.noreply.github.com>
2022-07-28 17:48:13 +0530

    Add files via upload

---
+++ DailyProgram/OperatorExample.java
@@ -0,0 +1,32 @@
+class OperatorExample
+
+{ 
+public static void main(String args[])  
+
+
+{
+
+int a=18;
+
+int b=7;
+
+System.out.print("The addition of two operands are: ");
+
+System.out.println(a+b);
+
+System.out.print("The Subtraction of two  operands are: ");
+
+System.out.println(a-b);
+
+System.out.print("The Multiplication of two operands are: ");
+
+System.out.println(a*b);
+
+double c= (double)a/b;
+
+System.out.print("The Division of two  operands are: ");
+
+System.out.println(c);
+
+}
+}
